79aed9067d7f8aee0c52cd03a2e10a80986a5eac
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-03 22:09:14 -0600

    * Added base app with svelte, typescript and tailwind * Added configuration for the code foundation

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,24 +1,39 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
   plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    [
+      "@snowpack/plugin-build-script",
+      { cmd: "postcss", input: [".css"], output: [".css"] },
+    ],
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+    "@snowpack/plugin-postcss",
+    [
+      "snowpack-plugin-rollup-bundle",
+      {
+        emitHtmlFiles: true,
+        preserveSourceFiles: false,
+        entrypoints: "build/**/*.js"
+      }
+    ],
   ],
   install: [
     /* ... */
   ],
   installOptions: {
+    installTypes: true,
     /* ... */
   },
   devOptions: {
     /* ... */
   },
   buildOptions: {
+    clean: true,
     /* ... */
   },
   proxy: {
