96efa9f74a9a568b804e240f482485e889d03931
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-15 15:27:33 -0600

    Added Home section

--- src/sections/Home.svelte
+++ src/sections/Home.svelte
@@ -1,1 +1,21 @@
-<div />
+<style>
+  #image-home {
+    background-image: url('/images/codigo.jpg');
+    background-repeat: no-repeat;
+    background-size: 800px 820px;
+    background-position: 70% 100%;
+  }
+</style>
+
+<div class="bg-gray-700 h-full text-white">
+  <div class="container h-full mx-auto flex justify-start items-center ">
+    <div id="image-home" class="w-1/2 mr-8 h-full" />
+    <div class="w-5/12">
+      <h3 class="text-5xl font-bold">Hi, I'm WillSnake</h3>
+      <p class="text-xl font-bold">a.k.a Daniel Rodríguez</p>
+      <p class="text-xl font-extralight">
+        I’m a Full-Stack Engineer mostly using Javascript, React JS, etc.
+      </p>
+    </div>
+  </div>
+</div>
