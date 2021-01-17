357dfc5b9b6d0ed156f56f3683a81a32f76016ab
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-17 12:22:52 -0600

    Created new components for the new About Page

---
+++ src/components/Education/SchoolDescription.svelte
@@ -0,0 +1,17 @@
+<script lang="typescript">
+  // Types
+  import type { SchoolDescription } from '../../interfaces/index'
+
+  // Props
+  export let schoolDescription: SchoolDescription = {
+    carrerTitle: 'Carrer Title',
+    dateRange: 'Date Range',
+    degree: 'Degree',
+  }
+</script>
+
+<div class="border-l-4 border-black py-1 pl-2 mb-6">
+  <h4 class="text-lg font-bold">{schoolDescription.degree}</h4>
+  <h5 class="text-base">{schoolDescription.dateRange}</h5>
+  <p>{schoolDescription.carrerTitle}</p>
+</div>
