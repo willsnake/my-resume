a4f4425e52e2b1dcc7eb0ec3befe187b74ab7586
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-23 19:52:05 -0600

    WIP for the home page

--- src/sections/Home.svelte
+++ src/sections/Home.svelte
@@ -1,15 +1,29 @@
+<style>
+  #me {
+    background-image: linear-gradient(
+        to left,
+        #111111 0%,
+        rgba(17, 17, 17, 0.0001) 100%
+      ),
+      url('https://res.cloudinary.com/willsnake/image/upload/v1610750180/codigo_adf7ic.jpg');
+    background-repeat: no-repeat;
+    background-size: 800px 690px;
+    background-position: 70% 100%;
+  }
+</style>
+
 <div id="home" class="bg-gray-700 h-full text-white">
-  <div class="container h-full mx-auto flex justify-start items-center ">
-    <div class="w-1/2 mr-8 h-full">
-      <picture>
+  <div class="h-full flex justify-between items-center ">
+    <div id="me" class="w-1/2 mr-8 h-full">
+      <!-- <picture id="me" class="">
         <source
           srcset="https://res.cloudinary.com/willsnake/image/upload/v1610750180/codigo_adf7ic.jpg"
           media="(min-width: 800px)" />
         <img
-          class="object-cover h-full"
+          class="object-cover h-full bg-gradient-to-r from-transparent to-black"
           src="https://res.cloudinary.com/willsnake/image/upload/v1610750180/codigo_adf7ic.jpg"
           alt="WillSnake coding" />
-      </picture>
+      </picture> -->
     </div>
     <div class="w-5/12">
       <h3 class="text-5xl font-bold">Hi, I'm WillSnake</h3>
