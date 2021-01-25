04f3ac414a80be6b29c033c40f97dcd662fa3a5a
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-24 21:28:18 -0600

    Finished twealking home page

--- src/sections/Home.svelte
+++ src/sections/Home.svelte
@@ -1,36 +1,54 @@
+<script lang="typescript">
+  // Interfaces
+  import type { ButtonProps } from '../interfaces'
+  import { ButtonType } from '../interfaces'
+
+  // Components
+  import Button from '../components/Button/index.svelte'
+
+  // Buttons Data
+  const contactMeButton: ButtonProps = {
+    text: 'Contact me',
+    type: ButtonType.PRIMARY,
+  }
+
+  const downloadResumeButton: ButtonProps = {
+    text: 'Download resume',
+    type: ButtonType.SECONDARY,
+  }
+</script>
+
 <style>
-  #me {
-    background-image: linear-gradient(
-        to left,
-        #111111 0%,
-        rgba(17, 17, 17, 0.0001) 100%
-      ),
-      url('https://res.cloudinary.com/willsnake/image/upload/v1610750180/codigo_adf7ic.jpg');
+  #home {
+    height: 492px;
+    background-image: url('https://res.cloudinary.com/willsnake/image/upload/v1611456206/willsnakeDev/hero-x2_onymw4.png');
     background-repeat: no-repeat;
-    background-size: 800px 690px;
-    background-position: 70% 100%;
+    background-size: 100% 492px;
+    background-position: 100%;
   }
 </style>
 
-<div id="home" class="bg-gray-700 h-full text-white">
-  <div class="h-full flex justify-between items-center ">
-    <div id="me" class="w-1/2 mr-8 h-full">
-      <!-- <picture id="me" class="">
-        <source
-          srcset="https://res.cloudinary.com/willsnake/image/upload/v1610750180/codigo_adf7ic.jpg"
-          media="(min-width: 800px)" />
-        <img
-          class="object-cover h-full bg-gradient-to-r from-transparent to-black"
-          src="https://res.cloudinary.com/willsnake/image/upload/v1610750180/codigo_adf7ic.jpg"
-          alt="WillSnake coding" />
-      </picture> -->
-    </div>
-    <div class="w-5/12">
-      <h3 class="text-5xl font-bold">Hi, I'm WillSnake</h3>
-      <p class="text-xl font-bold">a.k.a Daniel Rodríguez</p>
-      <p class="text-xl font-extralight">
-        I’m a Full-Stack Engineer mostly using Javascript, React JS, etc.
+<div id="home" class="flex justify-end items-center w-full mt-14">
+  <div class="w-1/2 h-1/2">
+    <div
+      class="flex flex-col space-y-4 items-start justify-start"
+      style="width: 386px; height: 196px;">
+      <p
+        class="text-3xl font-medium tracking-wider text-white"
+        style="width: 386px;">
+        Hi, I’m WillSnake
       </p>
+      <p class="text-xl tracking-wider text-white" style="width: 386px;">
+        a.k.a. Daniel Rodríguez
+      </p>
+      <p class="text-base tracking-wide text-gray-300" style="width: 386px;">
+        I’m a Full-Stack Engineer … React JS (cuáles otras tecnologías usas,
+        etc.)
+      </p>
+      <div class="inline-flex space-x-4 items-start justify-start">
+        <Button buttonProps={contactMeButton} />
+        <Button buttonProps={downloadResumeButton} />
+      </div>
     </div>
   </div>
 </div>
