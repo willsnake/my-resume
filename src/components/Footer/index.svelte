fadf4b9aa363ec3b92689dd577f5fdd86c96ce93
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-31 21:28:45 -0600

    Refactored footer

--- src/components/Footer/index.svelte
+++ src/components/Footer/index.svelte
@@ -1,12 +1,9 @@
-<div class="w-full bg-black h-14 text-white text-lg font-semibold">
-  <div class="container mx-auto">
-    <div
-      class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row">
-      <span class="h-full">WILLSNAKE</span>
-      <span class="h-full">Design by
-        <a
-          target="_blank"
-          href="https://dribbble.com/lilycaulfield">@lilycaulfield</a></span>
-    </div>
+<footer class="col-span-full bg-black h-14 text-white text-lg font-semibold">
+  <div class="container mx-auto flex px-4 md:items-center md:justify-between">
+    <span class="h-full">WILLSNAKE</span>
+    <span class="h-full">Design by
+      <a
+        target="_blank"
+        href="https://dribbble.com/lilycaulfield">@lilycaulfield</a></span>
   </div>
-</div>
+</footer>
