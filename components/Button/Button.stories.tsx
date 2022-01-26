24849144cfe7690aa63198f6fac5e872184e4528
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2022-01-26 12:27:57 -0600

    docs(components/Button/Button.stories.tsx): Setup better story for Button

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { Button } from './Button'
+import { VARIANTS_MAP } from './types'
 
 // // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -9,14 +10,15 @@ export default {
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    variant: { control: { type: 'select', options: Object.keys(VARIANTS_MAP) } },
   },
 } as ComponentMeta<typeof Button>
 
 // // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>My First Button</Button>
-)
+const Template: ComponentStory<typeof Button> = (args) => {
+  return (
+  <Button {...args}>This is a very large button</Button>
+)}
 
 export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
