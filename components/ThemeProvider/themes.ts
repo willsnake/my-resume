c154ded316db2c82fb7c47b535394a529f5d3ed3
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2022-01-26 19:57:11 -0600

    feat(components/ThemeProvider/themes.ts): Created base theme

---
+++ components/ThemeProvider/themes.ts
@@ -0,0 +1,16 @@
+import { Theme } from './types'
+
+const baseTheme: Theme = {
+    colors: {
+        black: '#111111',
+        darkGrey: '#666666',
+        lightGrey: '#EAEAEA',
+        midGrey: '#999999',
+        primary: '#254F85',
+        secondary: '#333333',
+        tertiary: '#1076F9',
+        white: '#FAFAFA', 
+    }
+}
+
+export { baseTheme }
